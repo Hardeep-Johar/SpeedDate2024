744b8204aaf773fc9b5cc7134d629eb27f8ef807
hardeep <hardeep.johar@gmail.com>
2016-05-25 15:09:04 -0400

    Added stuff for EB config

--- SpeedDate/settings.py
+++ SpeedDate/settings.py
@@ -86,9 +86,20 @@ DATABASES = {
     }
 }
 """
-
-DATABASES = {
-      'default': {
+if 'RDS_DB_NAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+    }
+else:
+    DATABASES = {
+        'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'speeddate',
             'USER': 'hardeepjohar',
@@ -133,7 +144,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,"..","www","static")
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/welcome'

7b62b97a254a11f8e68ad33d37510ab545933bb4
hardeep <hardeep.johar@gmail.com>
2016-05-25 14:35:15 -0400

    migrated to postgres

--- SpeedDate/settings.py
+++ SpeedDate/settings.py
@@ -78,13 +78,25 @@ WSGI_APPLICATION = 'SpeedDate.wsgi.application'
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 # The plan is to switch this to postgres
 # Come hell or high water
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
 
+DATABASES = {
+      'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'speeddate',
+            'USER': 'hardeepjohar',
+            'PASSWORD': 'speeddateguru',
+            'HOST': 'localhost',
+            'PORT': '5432',
+      }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
